d05caec99aed5e95539f6a75b714a887d9cbf876
willclarktech <will@willclark.tech>
2017-04-18 14:10:04 +0200

    make footer link an anchor

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import style from '../styles/Footer.style'
 
-export default ({ link }) => (
-  <footer style={ style }>
-    Made with React: source code at { link }
-  </footer>
-)
+export default ({ link }) => {
+  const href = link.indexOf('http') === 0
+    ? link
+    : `https://${ link }`
+  return (
+    <footer style={ style }>
+      { 'Made with React: source code at ' }
+      <a href={ href }>
+        { link }
+      </a>
+    </footer>
+  )
+}
