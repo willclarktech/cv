f36abc81c57ecc7afb223df12e8b9d40d5047be9
willclarktech <will@willclark.tech>
2017-04-19 02:07:23 +0200

    fix lint errors in stories

--- src/stories/index.js
+++ src/stories/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { storiesOf, action, linkTo } from '@kadira/storybook'
+import { storiesOf /*, action, linkTo */ } from '@kadira/storybook'
 import Intro from '../components/Intro'
 import Contact from '../components/Contact'
 import Section from '../components/Section'
